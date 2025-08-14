57d94c4d9660cdbb941d019a78a7abb43c59c9c7
Lateh Larry <clatehlarry@gmail.com>
2025-08-14 11:09:07 +0100

    refactoting agents

--- src/agents/sanita.py
+++ src/agents/sanita.py
@@ -1,7 +1,7 @@
 from .agent_base import AgentBase
 import os
 
-class SanitaAgent(AgentBase):
+class Sanita(AgentBase):
     def __init__(self):
         yaml_path = os.path.join(os.path.dirname(__file__), "agent_config/sanita.yml")
         super().__init__(yaml_path)
--- src/agents/uby.py
+++ src/agents/uby.py
@@ -1,7 +1,7 @@
 from .agent_base import AgentBase
 import os
 
-class UbyAgent(AgentBase):
+class Uby(AgentBase):
     def __init__(self):
         yaml_path = os.path.join(os.path.dirname(__file__), "agent_config/uby.yml")
         super().__init__(yaml_path)
