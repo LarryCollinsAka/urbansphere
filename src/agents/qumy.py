bf4309a20a82958aa9173c7b9c74ee0226cf3b11
Lateh Larry <clatehlarry@gmail.com>
2025-08-14 11:04:50 +0100

    refactoting agents

--- src/agents/qumy.py
+++ src/agents/qumy.py
@@ -1,28 +1,7 @@
-from src.agents.agent_base import AgentBase
-from src.models.watsonx_service import WatsonxService
-from src.models.db_models import Task, db
+from .agent_base import AgentBase
+import os
 
-class Qumy(AgentBase):
-    def __init__(self, watsonx_client: WatsonxService):
-        super().__init__(watsonx_client)
-
-    def add_task(self, sender_id: str, message_body: str, intent: str) -> Task:
-        new_task = Task(
-            sender_id=sender_id,
-            message_body=message_body,
-            intent=intent
-        )
-        db.session.add(new_task)
-        db.session.commit()
-        return new_task
-
-    def get_task(self, task_id: int) -> Task:
-        return Task.query.get(task_id)
-
-    def update_task_status(self, task_id: int, new_status: str):
-        task = self.get_task(task_id)
-        if task:
-            task.status = new_status
-            db.session.commit()
-            return True
-        return False
+class QumyAgent(AgentBase):
+    def __init__(self):
+        yaml_path = os.path.join(os.path.dirname(__file__), "agent_config/qumy.yml")
+        super().__init__(yaml_path)
